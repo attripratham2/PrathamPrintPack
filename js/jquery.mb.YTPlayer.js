2bdd4f3ab1539cb6ee234f4508a34a4cf1f3f5ca
attripratham2 <101089053+attripratham2@users.noreply.github.com>
2023-12-16 20:48:19 +0530

    Update jquery.mb.YTPlayer.js

--- js/jquery.mb.YTPlayer.js
+++ js/jquery.mb.YTPlayer.js
@@ -1,23 +1,3 @@
-/*
- * ******************************************************************************
- *  jquery.mb.components
- *  file: jquery.mb.YTPlayer.js
- *
- *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi);
- *  Open lab srl, Firenze - Italy
- *  email: matteo@open-lab.com
- *  site: 	http://pupunzi.com
- *  blog:	http://pupunzi.open-lab.com
- * 	http://open-lab.com
- *
- *  Licences: MIT, GPL
- *  http://www.opensource.org/licenses/mit-license.php
- *  http://www.gnu.org/licenses/gpl.html
- *
- *  last modified: 23/11/13 21.05
- *  *****************************************************************************
- */
-
 if(typeof ytp != "object")
 	ytp ={};
 
